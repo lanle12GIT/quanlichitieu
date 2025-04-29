51bb4e1e36d6a34a35c688aa56a69ad96fea4e4e
Tuan Nguyen <lanle120801@gmail.com>
2025-04-29 16:33:56 +0700

    tạo router điều hướng trang

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,44 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: < App/>,
+    // children: [
+    //   {
+    //     index: true,
+    //     element: <Home />,
+    //   },
+    //   {
+    //     path: "about",
+    //     element: <About />,
+    //   },
+    // ],
+  },
+  {
+    path: "/order",
+    element: <div>đơn hàng của bạn</div>,
+  },
+  {
+    path: "/spend",
+    element: <div>Chi tiêu của bạn</div>,
+  },
+  {
+    path: "/statistic",
+    element: <div>thống kê</div>,
+  },
+
+]);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
